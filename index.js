951bb77ec981a3daddd9b85cacb2bf5fedaf5ef2
Paul Rowan <prowan@novisci.com>
2021-03-11 16:47:38 -0500

    fix exports

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.compressExt = exports.parseUrl = exports.writeStreamUrl = exports.writerUrl = exports.readStreamUrl = exports.readerUrl = void 0;
-var reader_url_1 = require("./lib/reader-url");
-Object.defineProperty(exports, "readerUrl", { enumerable: true, get: function () { return reader_url_1.readerUrl; } });
-var readstream_url_1 = require("./lib/readstream-url");
-Object.defineProperty(exports, "readStreamUrl", { enumerable: true, get: function () { return readstream_url_1.readStreamUrl; } });
-var writer_url_1 = require("./lib/writer-url");
-Object.defineProperty(exports, "writerUrl", { enumerable: true, get: function () { return writer_url_1.writerUrl; } });
-var writestream_url_1 = require("./lib/writestream-url");
-Object.defineProperty(exports, "writeStreamUrl", { enumerable: true, get: function () { return writestream_url_1.writeStreamUrl; } });
-var parse_url_1 = require("./lib/parse-url");
-Object.defineProperty(exports, "parseUrl", { enumerable: true, get: function () { return parse_url_1.parseUrl; } });
-var compress_ext_1 = require("./lib/compress-ext");
-Object.defineProperty(exports, "compressExt", { enumerable: true, get: function () { return compress_ext_1.compressExt; } });
-// export {
-//   readerUrl, readStreamUrl,
-//   writerUrl, writeStreamUrl,
-//   parseUrl,
-//   compressExt
-// }
+const reader_url_1 = require("./lib/reader-url");
+const readstream_url_1 = require("./lib/readstream-url");
+const writer_url_1 = require("./lib/writer-url");
+const writestream_url_1 = require("./lib/writestream-url");
+const parse_url_1 = require("./lib/parse-url");
+const compress_ext_1 = require("./lib/compress-ext");
+module.exports = {
+    readerUrl: reader_url_1.readerUrl, readStreamUrl: readstream_url_1.readStreamUrl,
+    writerUrl: writer_url_1.writerUrl, writeStreamUrl: writestream_url_1.writeStreamUrl,
+    parseUrl: parse_url_1.parseUrl,
+    compressExt: compress_ext_1.compressExt
+};
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
-export { readerUrl } from './lib/reader-url'
-export { readStreamUrl } from './lib/readstream-url'
-export { writerUrl } from './lib/writer-url'
-export { writeStreamUrl } from './lib/writestream-url'
-export { parseUrl } from './lib/parse-url'
-export { compressExt } from './lib/compress-ext'
+import { readerUrl } from './lib/reader-url'
+import { readStreamUrl } from './lib/readstream-url'
+import { writerUrl } from './lib/writer-url'
+import { writeStreamUrl } from './lib/writestream-url'
+import { parseUrl } from './lib/parse-url'
+import { compressExt } from './lib/compress-ext'
 
-// export {
-//   readerUrl, readStreamUrl,
-//   writerUrl, writeStreamUrl,
-//   parseUrl,
-//   compressExt
-// }
+export = {
+  readerUrl, readStreamUrl,
+  writerUrl, writeStreamUrl,
+  parseUrl,
+  compressExt
+}
 
 
